0b5a6ef565cb7bb1d00fd87d586ec0130b07d7e1
MJWoov <jeminjae@naver.com>
2024-02-05 16:45:26 +0900

    1

--- 리스트.py
+++ 리스트.py
@@ -2,7 +2,24 @@ a=list()
 print(a)
 
 a=[1,2,3]
-print(a[-1])
+print(a[-1]) #인덱싱
+print(a[:2]) #슬라이싱
 
-a=[[1,2],[3,4],[5,6]]
-print(a[0][0])
+b=[[1,2],[3,4],[5,6]] #이중리스트
+print(b[0][0]) 
+
+print(a+b)
+print(a*2)
+len(b)
+
+a.append(1)
+print(a)
+a.sort()
+print(a)
+a.reverse()
+print(a)
+a.insert(0,4)
+a.pop()
+print(a)
+a.extend([4,5])
+
