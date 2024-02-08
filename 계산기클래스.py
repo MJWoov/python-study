23a9117a42f2c517a37e0bc2504063e6f60cf7fb
MJWoov <jeminjae@naver.com>
2024-02-08 10:22:57 +0900

    Update 계산기클래스.py

--- 계산기클래스.py
+++ 계산기클래스.py
@@ -1,4 +1,9 @@
 class FourCal():
+    mod = 1     #클래스변수
+    def __init__(self,first,second):   #생성자
+        self.first =first
+        self.second = second
+        
     def setdata(self,first,second):
         self.first = first
         self.second = second
@@ -11,13 +16,27 @@ class FourCal():
     def div(self):
         return self.first//self.second
 
+class MoreFourCal(FourCal):         #상속
+    def pow(self):
+        return self.first**self.second
 
-a = FourCal()
-a.setdata(3,4)
+class SafeFourCal(FourCal):         #메서드오버라이딩
+    def div(self):
+        if self.second ==0:
+            return 0
+        else : return self.first//self.second
+
+
+a = FourCal(3,4)
 print(a.add())
 print(a.div())
 print(a.sub())
 print(a.mul())
-b= FourCal()
-b.setdata(5,2)
+print(a.mod)
+b= FourCal(3,4)
 b.mul()
+print(b.mod)
+c=MoreFourCal(5,7)
+print(c.pow())
+d = SafeFourCal(3,0)
+print(d.div())
